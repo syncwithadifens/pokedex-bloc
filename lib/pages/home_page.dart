dfd0b64c64515aa81ab8d1eadff7fa6e93708e69
Afiv Dicky Efendy <dickyareda@gmail.com>
2022-08-29 21:14:34 +0700

    redesign homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,27 +8,64 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: BlocBuilder<PokeBloc, PokeState>(
-        builder: (context, state) {
-          if (state is PokeInitial) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is PokeLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is PokeLoaded) {
-            return ListView.builder(
-              itemCount: state.pokedata.length,
-              itemBuilder: (context, index) {
-                return Text(state.pokedata[index].name.toString());
-              },
-            );
-          } else {
-            return const Center(child: Text('no data'));
-          }
-        },
+      body: SafeArea(
+          child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'All Pokemon Character',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          BlocBuilder<PokeBloc, PokeState>(
+            builder: (context, state) {
+              if (state is PokeInitial) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is PokeLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is PokeLoaded) {
+                return Expanded(
+                  child: GridView.builder(
+                    padding: const EdgeInsets.all(10),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 20,
+                      crossAxisSpacing: 20,
+                    ),
+                    itemCount: state.pokedata.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: BorderRadius.circular(8)),
+                        child: Center(
+                          child: Text(
+                            state.pokedata[index].name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              } else {
+                return const Center(child: Text('no data'));
+              }
+            },
+          ),
+        ],
       )),
     );
   }
